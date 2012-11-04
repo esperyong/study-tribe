962813f4a5d649786f0b15bd99c58dce86c9ab21
liuyong <jordanyong@yahoo.com>
2012-11-04 10:48:43 +0800

    refractory use mixin and generics

--- webapp/studytribe-project/studytribe/studygroup/views.py
+++ webapp/studytribe-project/studytribe/studygroup/views.py
@@ -1,64 +1,44 @@
 # -*- coding:utf-8 -*-
-from rest_framework import status
-from rest_framework.response import Response
 from studytribe.studygroup.models import StudyGroup
 from studytribe.studygroup.serializers import StudyGroupSerializer
 from rest_framework import mixins
 from rest_framework import generics
-from rest_framework.views import APIView
-from django.http import Http404
 
 class StudyGroupListRes(mixins.ListModelMixin,
                         mixins.CreateModelMixin,
                         generics.MultipleObjectAPIView):
-    model = StudyGroup
-    serializer_class = StudyGroupSerializer
     """
     List all studygroup,or create a new studygroup
     """
-    def get(self,request,format=None):
-        groups = StudyGroup.objects.all()
-        serializer = StudyGroupSerializer(instance=groups)
-        return Response(serializer.data)
+
+    model = StudyGroup
+    serializer_class = StudyGroupSerializer
+
+    def get(self,request,*args,**kwargs):
+        return self.list(request,*args,**kwargs)
 
     def post(self,request,format=None):
-        serializer = StudyGroupSerializer(request.DATA)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
+        return self.create(request,*args,**kwargs)
 
     
-class StudyGroupRes(APIView):
+class StudyGroupRes(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.SingleObjectAPIView):
     """
     Retrieve,update or delete a studygroup
     """
-    def get_object(self,pk):
-        try:
-            return StudyGroup.objects.get(pk=pk)
-        except StudyGroup.DoesNotExist:
-            return Http404
+    model = StudyGroup
+    serializer_class = StudyGroupSerializer
 
-    def get(self,request,pk,format=None):
-        studygroup = self.get_object(pk)
-        serializer = StudyGroupSerializer(instance=studygroup)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def put(self,request,pk,format=None):
-        studygroup = self.get_object(pk)
-        serializer = StudyGroupSerializer(request.DATA,instance=studygroup)
-        if serializer.is_valid(): 
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
-    def delete(self,request,pk,format=None):
-        studygroup = self.get_object(pk)
-        studygroup.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
 
 
 
