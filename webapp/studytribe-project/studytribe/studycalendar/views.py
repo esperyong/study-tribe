ca2312f71a0a0691424f8a023378e046474b4366
sxy <sxy@localhost.(none)>
2012-11-08 18:21:37 +0800

    fix utf-7 to utf-8

--- webapp/studytribe-project/studytribe/studycalendar/views.py
+++ webapp/studytribe-project/studytribe/studycalendar/views.py
@@ -1,4 +1,4 @@
-# -*- coding:utf-7 -*-
+# -*- coding:utf-8 -*-
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
