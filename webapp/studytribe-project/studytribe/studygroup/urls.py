29ee71af809efccea744e85f8d0e2f7215cdc32f
刘 勇 <liuyong@192.168.0.150>
2012-11-13 10:06:06 +0800

    configurate url

--- webapp/studytribe-project/studytribe/studygroup/urls.py
+++ webapp/studytribe-project/studytribe/studygroup/urls.py
@@ -3,6 +3,7 @@
 from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
 from studytribe.studygroup import views 
+from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
     url(r'^$', views.StudyTribeListRes.as_view()),
@@ -16,5 +17,8 @@ urlpatterns = format_suffix_patterns(urlpatterns)
 
 urlpatterns += patterns('studytribe',  
     url(r'^studygroups/','studygroup.views.study_group_list',name='study-group-list'),
+    url(r'^tribemember/invite-people/$',direct_to_template,
+                  {'template': 'studytribe/tribemember/invite_member.html'},
+                  name='invite-tribe-member'),
 )
 
