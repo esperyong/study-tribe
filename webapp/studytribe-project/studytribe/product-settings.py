802be5e3f2c185a9edcd09810e4ff630a97547d4
liuyong <jordanyong@yahoo.com>
2013-01-06 23:01:46 +0800

    add product-settings,and add invitation app

---
+++ webapp/studytribe-project/studytribe/product-settings.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from studytribe.settings import *
+
+STATIC_ROOT = '/home/ubuntu/develop/statics/studytribe/static/'
+MEDIA_ROOT = '/home/ubuntu/develop/statics/studytribe/media/'
+
+DEBUG = False
+
